7712f765921c28db1bdbaedd52ec226ff8680a60
Simranbhandari1 <simranbhandari604@gmail.com>
2026-08-02 14:23:21 +0530

    changes

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -3,27 +3,19 @@ const cookieParser = require('cookie-parser');
 const cors = require('cors');
 
 const app = express();
-app.use((req, res, next) => {
-  console.log('REQUEST:', req.method, req.originalUrl);
-  next();
-});
+// app.use((req, res, next) => {
+//   console.log('REQUEST:', req.method, req.originalUrl);
+//   next();
+// });
 app.set('trust proxy', 1);
 app.use(express.json());
 app.use(cookieParser());
-// app.use(
-//   cors({
-//     origin:
-//       // [
-//       //   'http://localhost:5173',
-//       'https://ai-resume-analyzer-smoky-eight.vercel.app',
-//     //   'https://ai-resume-analyzer-7aw9co234-simrans-projects-654e77f7.vercel.app',
-//     // ],
-//     credentials: true,
-//   }),
-// );
 app.use(
   cors({
-    origin: 'https://ai-resume-analyzer-smoky-eight.vercel.app',
+    origin: [
+      'http://localhost:5173',
+      'https://ai-resume-analyzer-smoky-eight.vercel.app',
+    ],
     credentials: true,
   }),
 );
