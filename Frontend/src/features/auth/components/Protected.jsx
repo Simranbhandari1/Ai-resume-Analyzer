b069fd85c783854c6c23ed02b85751dcae48ed7a
Simranbhandari1 <simranbhandari604@gmail.com>
2026-08-02 23:36:33 +0530

    changes

--- Frontend/src/features/auth/components/Protected.jsx
+++ Frontend/src/features/auth/components/Protected.jsx
@@ -6,14 +6,14 @@ import React from 'react';
 const Protected = ({ children }) => {
   const { loading, user } = useAuth();
 
-  if (loading) {
-    return (
-      <main>
-        {/* <h1>Loading...</h1> */}
-        <Loader />
-      </main>
-    );
-  }
+  // if (loading) {
+  //   return (
+  //     <main>
+  //       {/* <h1>Loading...</h1> */}
+  //       <Loader />
+  //     </main>
+  //   );
+  // }
 
   if (!user) {
     return <Navigate to={'/login'} />;
